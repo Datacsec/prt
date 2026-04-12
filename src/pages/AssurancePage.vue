29db923e2587adeef7462a4859d43d9d1ad1a19c
Datacsec <132220678+Datacsec@users.noreply.github.com>
2026-04-12 23:59:39 +0100

    Update AssurancePage.vue

--- src/pages/AssurancePage.vue
+++ src/pages/AssurancePage.vue
@@ -1,4 +1,4 @@
-M8 10h.01M12 10h.01M16 10h.01M9 16H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-5l-5 5v-5z
+d="M3 6l3 1m0 0l-3 9a5.002 5.002 0 006.001 0M6 7l3 9M6 7l6-2m6 2l3-1m-3 1l-3 9a5.002 5.002 0 006.001 0M18 7l3 9m-3-9l-6-2m0-2v2m0 16V5m0 16H9m3 0h3"
 
 <template>
   <div class="flex flex-col flex-1 min-h-0 w-full h-full">
